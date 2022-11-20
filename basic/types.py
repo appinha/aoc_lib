4084f20ebd13ac5e6fabad4ebc7242826d128ba7
appinha <amanda_pinha@hotmail.com>
2022-11-20 16:26:22 -0300

    [basic][types] Minor revisions

--- basic/types.py
+++ basic/types.py
@@ -1,20 +1,26 @@
 def is_int(object: object):
     return (isinstance(object, int))
 
+
 def is_float(object: object):
     return (isinstance(object, float))
+
 
 def is_number(object: object):
     return is_int(object) or is_float(object)
 
+
 def is_str(object: object):
     return (isinstance(object, str))
+
 
 def is_list(object: object):
     return (isinstance(object, list))
 
+
 def is_dict(object: object):
     return (isinstance(object, dict))
+
 
 def str_to_int(s: str):
     if is_str(s) and s.isdigit():
