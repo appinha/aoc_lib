cad999b9165145dcc6289c0f1702635ef25705df
appinha <amanda_pinha@hotmail.com>
2022-11-20 16:25:57 -0300

    [basic][iters] Adds list_combinations + minor revisions

--- basic/iters.py
+++ basic/iters.py
@@ -1,13 +1,18 @@
 import itertools
 
 
-def count_occurrences(iterator):
+def count_occurrences(iterable):
     return [
         {"item": k, "occurrences": len(list(v))}
-        for k, v in itertools.groupby(iterator)
+        for k, v in itertools.groupby(iterable)
     ]
 
-def get_unique_permutations(elements):
+
+def list_combinations(iterable, n):
+    return list(itertools.combinations(iterable, n))
+
+
+def list_unique_permutations(elements):
     reversed_permutations = set()
     unique_permutations = []
     for permutation in itertools.permutations(elements):
