716c71e4999a0edaeefdf305ba48491ad2b71085
appinha <amanda_pinha@hotmail.com>
2022-11-28 15:05:57 -0300

    [basic] Adds math

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from aoc_lib.basic.iters import *
+from aoc_lib.basic.math import *
 from aoc_lib.basic.types import *
---
+++ basic/math.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def prod(array_like_obj):
+    return np.prod(array_like_obj)
