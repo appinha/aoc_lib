06fc97309b51bd8549459cab65d67b0f21e08685
appinha <amanda_pinha@hotmail.com>
2022-11-29 20:17:40 -0300

    [basic][iters] Adds cycle

--- basic/iters.py
+++ basic/iters.py
@@ -1,18 +1,22 @@
 import itertools
+import typing as t
 
 
-def count_occurrences(iterable):
+def cycle(iterable: t.Iterable):
+    return itertools.cycle(iterable)
+
+def count_occurrences(iterable: t.Iterable):
     return [
         {"item": k, "occurrences": len(list(v))}
         for k, v in itertools.groupby(iterable)
     ]
 
 
-def list_combinations(iterable, n):
+def list_combinations(iterable: t.Iterable, n: int):
     return list(itertools.combinations(iterable, n))
 
 
-def list_unique_permutations(elements):
+def list_unique_permutations(elements: t.Iterable):
     reversed_permutations = set()
     unique_permutations = []
     for permutation in itertools.permutations(elements):
