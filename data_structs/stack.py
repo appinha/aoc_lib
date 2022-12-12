5280df384c8892808223b023425ebc67b926fcd2
appinha <amanda_pinha@hotmail.com>
2022-12-12 19:31:10 -0300

    [data_stucts][stack] Some minor additions and revisions

--- data_structs/stack.py
+++ data_structs/stack.py
@@ -1,18 +1,19 @@
-from typing import TypeVar, Generic, List
-
-
-T = TypeVar('T')
+from aoc_lib.basic.types import *
 
 
 class Stack(Generic[T]):
     def __init__(self) -> None:
         self.items: List[T] = []
 
+    def __repr__(self) -> str:
+        return repr(self.items)
+
+    @property
+    def empty(self) -> bool:
+        return not self.items
+
     def push(self, item: T) -> None:
         self.items.append(item)
 
     def pop(self) -> T:
         return self.items.pop()
-
-    def __repr__(self) -> str:
-        return repr(self.items)
