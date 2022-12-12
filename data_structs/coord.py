69ca6b40bdd6393bab2dccdfd4dd1caf59feb354
appinha <amanda_pinha@hotmail.com>
2022-12-12 17:33:17 -0300

    [data_structs][coord] Some minor revisions

--- data_structs/coord.py
+++ data_structs/coord.py
@@ -2,19 +2,32 @@ from aoc_lib.basic.regex import find_all_integers
 
 
 class Coord2D():
-    def __init__(self, data: list[int] | str):
+    def __init__(self, data: tuple[int, int] | list[int] | str):
         self.x, self.y = Coord2D.get(data)
-        self.list = [self.x, self.y]
-        self.tuple = (self.x, self.y)
 
     def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
         return f"({self.x},{self.y})"
 
-    def get(data: list[int] | str) -> tuple[int, int]:
-        if type(data) == list:
+    @property
+    def row(self):
+        return self.x
+
+    @property
+    def col(self):
+        return self.y
+
+    @property
+    def as_list(self):
+        return [self.x, self.y]
+
+    @property
+    def as_tuple(self):
+        return (self.x, self.y)
+
+    def get(data: tuple[int, int] | list[int] | str) -> tuple[int, int]:
+        if type(data) == tuple:
+            return data
+        elif type(data) == list:
             return tuple(data)
         elif type(data) == str:
             return tuple(find_all_integers(data))
