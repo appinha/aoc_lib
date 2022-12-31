0d2a407bc2b5d918ec08655063862e12952228c6
appinha <amanda_pinha@hotmail.com>
2022-12-31 17:38:41 -0300

    [imports] Adds ast import

--- imports.py
+++ imports.py
@@ -1,3 +1,3 @@
-import collections, copy, functools, heapq, itertools, math, numpy, string
+import ast, collections, copy, functools, heapq, itertools, math, numpy, string
 from pprint import pprint
 from aoc_lib.basic.types import *
