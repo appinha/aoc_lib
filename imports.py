aec6c153e07ed425b00e6f7be75f56eb13c6e063
appinha <amanda_pinha@hotmail.com>
2023-01-02 09:18:13 -0300

    [imports] Adds re module

--- imports.py
+++ imports.py
@@ -1,3 +1,3 @@
-import ast, collections, copy, functools, heapq, itertools, math, numpy, string
+import ast, collections, copy, functools, heapq, itertools, math, numpy, re, string
 from pprint import pprint
 from aoc_lib.basic.types import *
